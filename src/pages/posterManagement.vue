290c2e6081205361b38436fb922a04a62f6be4fc
123123 <123123>
2019-07-29 16:55:52 +0800

    提交图片上传

--- src/pages/posterManagement.vue
+++ src/pages/posterManagement.vue
@@ -6,21 +6,24 @@
     </el-col>
 
     <!--列表-->
-    <el-table :data="ggArray" :border="true"  stripe highlight-current-row v-loading="listLoading" style="width: 100%;">
-
-      <el-table-column prop="content"  :show-overflow-tooltip="true" align="center" label="广告名称">
-      </el-table-column>
+    <el-table
+      :data="ggArray"
+      :border="true"
+      stripe
+      highlight-current-row
+      v-loading="listLoading"
+      style="width: 100%;"
+    >
+      <el-table-column prop="content" :show-overflow-tooltip="true" align="center" label="广告名称"></el-table-column>
       <el-table-column label="广告地址">
         <template width="90" scope="scope">
-        <img style="width:160px;height:80px;border:none;" :src="scope.row.link">
-        </template> 
+          <img style="width:160px;height:80px;border:none;" :src="scope.row.link" />
+        </template>
       </el-table-column>
 
-      <el-table-column prop="createAuthor"  :show-overflow-tooltip="true" align="center" label="创建人">
-      </el-table-column>
+      <el-table-column prop="createAuthor" :show-overflow-tooltip="true" align="center" label="创建人"></el-table-column>
 
-      <el-table-column prop="createDate"   align="center" label="创建时间" sortable>
-      </el-table-column>
+      <el-table-column prop="createDate" align="center" label="创建时间" sortable></el-table-column>
 
       <el-table-column align="center" width="420" fixed="right" label="操作">
         <template slot-scope="scope">
@@ -32,21 +35,32 @@
           >
             <i class="el-icon-edit-outline" style="margin-right: 5px"></i>修改广告
           </el-button>
+          <el-button
+            round
+            type="text"
+            style="color: #e15939"
+            @click="delDepartment(scope.$index, scope.row)"
+          >
+            <i class="el-icon-delete" style="margin-right: 5px"></i>删除广告
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
 
     <!--工具条-->
-    <el-col :span="24" class="toolbar toolbar_page" v-if="ggArray !== undefined  &&  ggArray.length > 0 ">
-    <el-pagination
-      :current-page="currentPage"
-      :page-sizes="[10, 20, 30, 40]"
-      :page-size="size"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="total">
-    </el-pagination>
-  </el-col>
-
+    <el-col
+      :span="24"
+      class="toolbar toolbar_page"
+      v-if="ggArray !== undefined  &&  ggArray.length > 0 "
+    >
+      <el-pagination
+        :current-page="currentPage"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size="size"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
+    </el-col>
 
     <!--修改广告"界面-->
     <el-dialog title="修改广告" :visible.sync="editFormVisible" :modal-append-to-body="false">
@@ -54,23 +68,26 @@
         <el-form-item label="	广告名称" prop="content">
           <el-input v-model="editForm.content" auto-complete="off"></el-input>
         </el-form-item>
-         <el-form-item label="外部链接" prop="imgPath">
+        <el-form-item label="外部链接" prop="imgPath">
           <el-input v-model="editForm.imgPath" auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="	广告地址" prop="link">
+        <el-form-item label="	广告地址" prop="link" style="position: relative;">
+          
           <el-upload
-              action='/api/common/upload'
-              :show-file-list="true"
-              :file-list="linkPic"
-              :v-model="editForm.linkPic"
-              list-type="picture-card"
-              :on-preview="handlePictureCardPreview"
-              :on-success="handleAvatarSuccess"
-              :on-remove="handleRemove"
-              :on-exceed="handleExceed"
-              :limit='1'
-            >
-          <i class="el-icon-plus"></i>
+            action="/api/common/upload"
+            :show-file-list="istrue"
+            :file-list="linkPic"
+            :v-model="editForm.link"
+            list-type="picture-card"
+            :on-preview="handlePictureCardPreview"
+            :on-success="handleAvatarSuccess"
+            :on-remove="handleRemove"
+            :on-exceed="handleExceed"
+            :limit="1"
+          >
+            
+            <img width="100%" class="imgg" ref="img1" :src="dialogImageUrl" alt/>
+            <i class="el-icon-plus"></i>
           </el-upload>
         </el-form-item>
         <el-form-item label="创建人" prop="createAuthor">
@@ -90,6 +107,7 @@
 export default {
   data() {
     return {
+      istrue: "",
       name: "",
       imgPath: "",
       link: "",
@@ -104,84 +122,136 @@ export default {
       currentPage: 1,
       listLoading: false,
       ggArray: [],
-      linkPic: "",
-      path: "",
+      linkPic:[],
       dialogImageUrl: "",
       dialogVisible: false,
       editFormVisible: false, // 修改界面是否显示,
       // 修改界面数据
-      editForm: {},
+      editForm: {}
     };
   },
   methods: {
     // 获取广告广告列表
-    getyyList () {
-      var that = this
-      that.$http.get('/api' + `/advertisement/getAdvertisementList`)
+    getyyList() {
+      var that = this;
+      that.$http
+        .get("/api" + `/advertisement/getAdvertisementList`)
         .then(res => {
-          console.log(res.data, '获取广告列表')
+          console.log(res.data, "获取广告列表");
           that.ggArray = res.data;
         })
         .catch(err => {
           console.log(err);
         });
     },
-
+    // 删除广告
+    delDepartment(s1, s2) {
+      console.log(s2);
+      this.$confirm("此操作将删除该广告, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "error"
+      })
+        .then(() => {
+          this.$http
+            .post("/api" + `/advertisement/deleteAdvertisement?id=${s2.id}`)
+            .then(res => {
+              if (res.data) {
+                this.$message.success("删除广告成功");
+                this.getyyList();
+                this.editFormVisible = false;
+              } else {
+                this.$message.error("删除广告失败");
+                this.editFormVisible = false;
+              }
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        })
+        .catch(() => {});
+    },
     // 修改广告弹窗
     handleEdit: function(s1, s2) {
-      console.log("修改数据:---->" + s2.link); 
-      this.editFormVisible = true
-      this.editForm = s2;    
+      console.log(s2)
+      console.log("修改数据:---->" + s2.link);
+      this.editFormVisible = true;
+      this.editForm = s2;
+      if(s2.link) {
+        this.dialogImageUrl = s2.link;
+        this.editFormVisible = true;
+        if(this.dialogImageUrl&&this.editForm.link){
+        this.editForm.link = s2.link
+        }
+      }else{
+        this.dialogImageUrl = s2.link;
+        
+      }
+
+      
     },
     // 修改广告
     upDateDepartment() {
-      
+      console.log(this.editForm);
+      const arr = this.editForm.link;
+      for (let i = 0; i < arr.length; i++) {
+        this.editForm.link = arr[i].response.data.url;
+      }
       this.$http
         .post("/api" + `/advertisement/updateAdvertisement`, this.editForm)
         .then(res => {
+            console.log(res.data)
           if (res.data) {
             this.$message.success("修改广告成功");
             this.getyyList();
+            this.dialogImageUrl = "";
             this.editFormVisible = false;
           } else {
             this.$message.error("修改广告失败");
             this.editFormVisible = false;
           }
+            this.editForm ={}
         })
         .catch(err => {
           console.log(err);
         });
     },
-    handleAvatarSuccess (res, file, fileList) {
+    handleAvatarSuccess(res, file, fileList) {
+      this.dialogImageUrl = "";
       this.linkPic = fileList;
       this.editForm.link = fileList;
     },
-    handlePictureCardPreview (file) {
-      this.dialogImageUrl = file.url
-      this.dialogVisible = true
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
     },
-    handleRemove (file, fileList) {
-      this.linkPic = fileList
+    handleRemove(file, fileList) {
+      this.linkPic = fileList;
     },
-    handleExceed  (files, fileList) {
-      this.$message.warning(`列表图最多上传1张`)
+    handleExceed(files, fileList) {
+      this.$message.warning(`列表图最多上传1张`);
+
     },
     handlePreview(file) {
       console.log(file);
     }
   },
-  mounted () {
-    
-    
-  },
-  created () {
-    this.getyyList()
+  mounted() {},
+  created() {
+    this.getyyList();
   }
-}
-
+};
 </script>
 
 <style scoped>
+.imgg {
+  width: 148px;
+  height: 148px;
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
 .table_container {
   /*padding: 30px 50px 80px 50px;*/
 }
